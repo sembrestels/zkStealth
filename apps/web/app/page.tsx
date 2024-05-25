1f360b8facabf0ffff201772639d70c0f4053134
Sem <931684+sembrestels@users.noreply.github.com>
2024-05-25 18:33:19 +0200

    Add fluidkey and get 30 stealth addresses in the main page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,41 @@
+"use client"
+import { useEffect, useState } from 'react';
+  import { runExample } from "@repo/fluidkey-utils";
 
-export default function Page(): JSX.Element {
-  return (
-    <main>
-      Hello Anon
-    </main>
-  );
-}
+  export default function Page(): JSX.Element {
+    const [results, setResults] = useState<{ nonce: bigint; stealthSafeAddress: string }[]>([]);
+
+    useEffect(() => {
+      async function fetchResults() {
+        const exampleResults = await runExample();
+        setResults(exampleResults);
+      }
+      fetchResults();
+    }, []);
+
+    return (
+      <main>
+        Hello Anon
+        {results.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Nonce</th>
+                <th>Stealth Safe Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map(({ nonce, stealthSafeAddress }) => (
+                <tr key={nonce.toString()}>
+                  <td>{nonce.toString()}</td>
+                  <td>{stealthSafeAddress}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </main>
+    );
+  }
